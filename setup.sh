02961050e4df2037992f00701cf1697cc6523fd3
Ashley Blackmore <ablackmore@atlassian.com>
2015-04-08 21:27:06 +0200

    add hook insertion safeguards and set up template dirs properly

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 #
 # ~BEARDSNAPPER~
 #
-# Idea taken from Víctor Martínez: https://github.com/knoopx
+# Inspired by Víctor Martínez: https://github.com/knoopx
+# Template checks are based on code from Ionica Bizau https://github.com/IonicaBizau
 # Incorporates changes by iHiD (https://gist.github.com/3207743)
+#
 # Find this script on github at https://github.com/ashleyblackmore/beardsnapper
+#
 # Warning: this has only been tested on Ubuntu!
 
 func_pause() {
@@ -38,6 +41,11 @@ echo
 echo 'Additionally, this script requires sudo/root user privilege, in order to install streamer.'
 func_pause 'Press [Enter] key to continue...'
 
+if ! haveProg sed ; then
+    echo 'Could not find perl Please install before executing this script again.'
+fi
+
+
 if haveProg apt-get ; then func_apt-get
 elif haveProg pacman ; then func_pacman
 elif haveProg yum ; then func_yum
@@ -46,21 +54,46 @@ else
     echo 'No package manager found. Please install streamer before executing this script again.'
 fi
 
-mkdir -pv ~/.git/hooks/post-commit.d/
-BSNAP=~/.git/hooks/post-commit.d/beardsnapper
-touch $BSNAP
+git_templates_dir=$(git config --global --get init.templatedir);
+if [ $? -ne 0 ]; then
+    # Create a new global templatedir if there are none
+    git_templates_dir="${HOME}/.git-templates"
+    git config --global init.templatedir "$git_templates_dir" && echo "Set new global git template dir at ${git_templates_dir}"
+fi
+git_hooks_dir="${git_templates_dir}/hooks"
+post_commit_path="${git_hooks_dir}/post-commit"
 
-echo -e '#!/bin/bash -x' > $BSNAP
-echo -e 'BSNAP_IMAGE_DIR=~/beardsnaps' >> $BSNAP
-echo -e 'mkdir -p $BSNAP_IMAGE_DIR' >> $BSNAP
-echo -e 'forked_image () {' >> $BSNAP
-echo -e '    streamer -q -s 1920x1080 -c /dev/video0 -j 100 -b 16 -o $BSNAP_IMAGE_DIR/$(date +%s)_$(basename $PWD).jpeg &' >> $BSNAP
-echo -e '    echo "Beardsnap saved to $BSNAP_IMAGE_DIR/"' >> $BSNAP
-echo -e '}' >> $BSNAP
-echo -e '' >> $BSNAP
-echo -e 'set -x' >> $BSNAP
-echo -e 'forked_image &' >> $BSNAP
+mkdir -p "$git_hooks_dir"
 
-chmod +x $BSNAP
+BSNAP_HOOK=$(cat <<EOF
+### beardsnapper hook (begin) ###
+# make the image dir
+BSNAP_IMAGE_DIR=~/beardsnaps
+mkdir -p $BSNAP_IMAGE_DIR
+
+forked_image () {
+    # take a picture from the webcam
+    streamer -q -s 1920x1080 -c /dev/video0 -j 100 -b 16 -o $BSNAP_IMAGE_DIR/$(date +%s)_$(basename $PWD).jpeg &
+    echo "Beardsnap saved to $BSNAP_IMAGE_DIR/"
+}
+
+set -x
+forked_image &
+### beardsnapper hook (end) ###
+EOF
+);
+
+if [ ! -f "$post_commit_path" ]; then
+    printf "#!/bin/sh\n%s" "$BSNAP_HOOK" > "$post_commit_path" \
+        && chmod +x "$post_commit_path" \
+        && echo "Successfully set up beardsnapper hook at ${post_commit_path}." \
+        && exit 0
+else
+    # Remove any previous git-stats hook code blocks
+    sed -i '/### beardsnapper hook (begin) ###/,/### beardsnapper hook (end) ###/d' $post_commit_path
+    printf "%s\n" "$BSNAP_HOOK" >> "$post_commit_path" \
+        && echo "Successfully set up beardsnapper hook at ${post_commit_path}." \
+        && exit 0
+fi
 
 git config --global init.templatedir '~/.git'
